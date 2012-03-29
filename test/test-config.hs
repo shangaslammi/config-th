c070439099fd4dfc1dc6fdcc0d8dff7c597101af
Sami Hangaslammi <sami.hangaslammi@leonidasoy.fi>
2012-03-29 17:08:17 +0300

    added test for parsing simple algeraic data type values

--- test/test-config.hs
+++ test/test-config.hs
@@ -13,43 +13,63 @@ data ConfTest1 = ConfTest1
     , optionalInt :: Maybe Int
     }
 
-mkConfig ''ConfTest1
+data ConfTest2 = ConfTest2
+    { customField1 :: MyType
+    , customField2 :: MyType
+    , customField3 :: MyType
+    }
 
-confTest1 = unlines $
+data MyType = Foo | Bar | Asdf deriving (Show, Eq)
+
+mkConfig ''ConfTest1
+mkConfig ''ConfTest2
+
+confTest1_1 = unlines $
     [ "stringField = foobar"
     , "intField    = 10"
     , "floatField  = 2.5"
     , "optionalInt = 4"
     ]
 
-confTest2 = unlines $
+confTest1_2 = unlines $
     [ "stringField = foobar"
     , "intField    = 10"
     , "floatField  = 2.5"
     ]
 
-confTest3 = unlines $
+confTest1_3 = unlines $
     [ "stringField = foobar"
     , "intField    = 10"
     ]
 
+confTest2_1 = unlines $
+    [ "customField1 = foo"
+    , "customField2 = bar"
+    , "customField3 = asdf"
+    ]
 
 main = hspec $ describe "Config-TH" $
     [ it "can parse basic named fields" $ do
-        let (Right cfg) = parseConfig confTest1
+        let (Right cfg) = parseConfig confTest1_1
         (cfg @@ stringField) @?= "foobar"
         (cfg @@ intField)    @?= 10
         (cfg @@ floatField)  @?= 2.5
         (cfg @@ optionalInt) @?= Just 4
 
     , it "returns missing optional fields as Nothing" $ do
-        let (Right cfg) = parseConfig confTest2
+        let (Right cfg) = parseConfig confTest1_2
         (cfg @@ stringField) @?= "foobar"
         (cfg @@ intField)    @?= 10
         (cfg @@ floatField)  @?= 2.5
         (cfg @@ optionalInt) @?= Nothing
 
     , it "reports an error for missing mandatory fields" $ do
-        let (Left err) = parseConfig confTest3 :: Either ConfigError ConfTest1
+        let (Left err) = parseConfig confTest1_3 :: Either ConfigError ConfTest1
         err @?= MissingField "floatField"
+
+    , it "can parse custom algebraic data types" $ do
+        let (Right cfg) = parseConfig confTest2_1
+        (cfg @@ customField1) @?= Foo
+        (cfg @@ customField2) @?= Bar
+        (cfg @@ customField3) @?= Asdf
     ]
